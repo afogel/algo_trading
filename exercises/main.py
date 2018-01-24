aaed8f86ab993f8cccdfa5b5f0857b61cd99f438
Ariel Fogel <fogeltine@gmail.com>
2018-01-24 15:52:47 -0800

    move helpers into separate dataframe_helpers directory

--- exercises/main.py
+++ exercises/main.py
@@ -1,10 +1,10 @@
-from dataframe_builder import DataframeBuilder
+from dataframe_helpers.dataframe_builder import DataframeBuilder
+from dataframe_helpers.dataframe_plotter import DataframePlotter
 
-print('In the main!')
+print('Starting Program!')
+
 date_range = ['12/01/2017', '01/16/2018']
 datasets = ['SPY', 'GLW', 'PHO', 'PALL', 'CEF']
 dataframe = DataframeBuilder(datasets, date_range).build()
-print(dataframe)
-
-# plot_labels = {'xlabel': 'Dates', 'ylabel': 'Price'}
-# dfp = DataframePlotter(dataframe, plot_labels).plot()
+plot_labels = {'xlabel': 'Dates', 'ylabel': 'Price'}
+dfp = DataframePlotter(dataframe, plot_labels).plot()
