53cf560ee5d4d7ee590409f62b7135de94f1924c
Ariel Fogel <fogeltine@gmail.com>
2018-01-25 15:27:31 -0800

    basic implementation of bollinger bands

--- exercises/main.py
+++ exercises/main.py
@@ -1,10 +1,13 @@
 from dataframe_helpers.dataframe_builder import DataframeBuilder
 from dataframe_helpers.dataframe_plotter import DataframePlotter
+from technical_indicators.bollinger_bands_plotter import BollingerBandsPlotter
 
 print('Starting Program!')
 
 date_range = ['12/01/2017', '01/16/2018']
-datasets = ['SPY', 'GLW', 'PHO', 'PALL', 'CEF']
+# datasets = ['SPY', 'GLW', 'PHO', 'PALL', 'CEF']
+datasets = ['SPY']
 dataframe = DataframeBuilder(datasets, date_range).build()
-plot_labels = {'xlabel': 'Dates', 'ylabel': 'Price'}
-dfp = DataframePlotter(dataframe, plot_labels).plot()
+# plot_labels = {'xlabel': 'Dates', 'ylabel': 'Price'}
+# dfp = DataframePlotter(dataframe, plot_labels).plot()
+BollingerBandsPlotter(dataframe, window=20).plot()
--- exercises/technical_indicators/bollinger_bands_plotter.py
+++ exercises/technical_indicators/bollinger_bands_plotter.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plot
 
-class BollingerBandPlotter():
+class BollingerBandsPlotter():
 	"""
 	BollingerBandPlotter will plot the bollinger bands (R) for a series of stocks contained
 	within a dataframe.
@@ -7,6 +8,25 @@ class BollingerBandPlotter():
 	given stock. The bollinger hypothesis is that if the current price of a stock is above or 
 	below a bollinger band, it signals a buy or sell signal.
 	"""
-	def __init__(self, arg):
-		self.arg = arg
+	def __init__(self, dataframe, window):
+		self.dataframe = dataframe
+		self.window = window
+
+	def plot(self):
+		mean = self._calculate_rolling_mean()
+		upperbound, lowerbound = self._calculate_bollinger_bands(mean)
+		axis = self.dataframe.plot(fontsize=12, title='Bollinger Bands')
+		mean.plot(label='Rolling Mean', ax=axis)
+		upperbound.plot(label='upper band', ax=axis)
+		lowerbound.plot(label='lower band', ax=axis)
+		plot.show()
+
+	def _calculate_rolling_mean(self):
+		return self.dataframe.rolling(window = self.window).mean()
+
+	def _calculate_bollinger_bands(self, mean):
+		standard_deviation = self.dataframe.rolling(window = self.window).std()
+		upperbound = mean + (2*standard_deviation)
+		lowerbound = mean - (2*standard_deviation)
+		return upperbound, lowerbound
 		
